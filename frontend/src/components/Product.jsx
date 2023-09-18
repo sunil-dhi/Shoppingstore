b22f54a2e05e7b96e32df9ca2ffab1290d18a97b
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-09-17 23:55:44 -0400

    producr list altered

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,21 +1,35 @@
-import styled from 'styled-components'
-const Container=styled.div`
-    
-`
-const Circle=styled.div`
-`
-const Image=styled.img`
-`
-const Info=styled.div`
-`
-const Icon=styled.div`
-`
-const Product = ({item}) => {
+import {
+  FavoriteBorderOutlined,
+  SearchOutlined,
+  ShoppingBasketOutlined,
+} from "@mui/icons-material";
+import styled from "styled-components";
+const Container = styled.div``;
+const Circle = styled.div``;
+const Image = styled.img``;
+const Info = styled.div``;
+const Icon = styled.div``;
+const Product = ({ item }) => {
   return (
-      <Container>
-       
-      </Container>
-  )
-}
+    <Container>
+      <Circel />
+      <Image />
+      <Info>
+        <Icon>
+          
+          <ShoppingBasketOutlined />
+        </Icon>
+        <Icon>
+          
+          <SearchOutlined />
+        </Icon>
+        <Icon>
+          
+          <FavoriteBorderOutlined />
+        </Icon>
+      </Info>
+    </Container>
+  );
+};
 
-export default Product
+export default Product;

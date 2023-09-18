b13ad6ff3c6e31a0f653fe162d78f2839d30526e
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-09-18 03:29:09 -0400

    second page is done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Announcement from '../components/Announcement'
-import Slider from '../components/Slider'
-import Categories from '../components/Categories'
-import Products from '../components/Products'
-import NewsLetter from '../components/NewsLetter'
-import Footer from '../components/Footer'
+// import Navbar from '../components/Navbar'
+// import Announcement from '../components/Announcement'
+// import Slider from '../components/Slider'
+// import Categories from '../components/Categories'
+// import Products from '../components/Products'
+// import NewsLetter from '../components/NewsLetter'
+// import Footer from '../components/Footer'
+import ProductList from './ProductList'
 const Home = () => {
   return (
     <>
-     <Announcement />
-      <Navbar />
-      <Slider />
-      <Categories />
-      <Products />
-      <NewsLetter />
-      <Footer />
+     <ProductList />
     </>
   )
 }

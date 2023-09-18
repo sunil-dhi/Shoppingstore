d774dd403f2f5a4744433a49b6499fed3d26eb74
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-09-18 00:49:31 -0400

    footer design added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Slider from '../components/Slider'
 import Categories from '../components/Categories'
 import Products from '../components/Products'
 import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer'
 const Home = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const Home = () => {
       <Categories />
       <Products />
       <NewsLetter />
+      <Footer />
     </>
   )
 }

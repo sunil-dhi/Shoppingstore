8af57feefdf491e17e23f537c4caee502da72690
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-09-18 00:18:47 -0400

    product component implemented

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -12,7 +12,7 @@ const Categories = () => {
     <Container>
       {
         categories.map(item=>(
-          <CategoryItem item={item}/>  
+          <CategoryItem item={item} key={item.id}/>  
         ))
       }
     </Container>
